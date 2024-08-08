6a57ae025279919f5a9ec384ce4bb61f548edd50
Jihwan Kim <jihwan.kim@purple.io>
2024-08-09 03:24:11 +0900

    Initial Commit

---
+++ card/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  card
+//
+//  Created by 김지환 on 8/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ card/cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardApp.swift
+//  card
+//
+//  Created by 김지환 on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
